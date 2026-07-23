10afc21f6f2db1cf355fb87a8ccf8ed981d6b72a
umarchon.nurmadov <umarnurmadov12@gmail.com>
2026-07-23 10:31:32 +0500

    Initial commit

---
+++ app/src/main/java/tj/daftariqarzho/app/ui/theme/Color.kt
@@ -0,0 +1,46 @@
+package tj.daftariqarzho.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+object DaftarColors {
+    val PrimaryLight = Color(0xFF2E7D32)
+    val OnPrimaryLight = Color(0xFFFFFFFF)
+    val PrimaryContainerLight = Color(0xFFA5D6A7)
+    val OnPrimaryContainerLight = Color(0xFF1B5E20)
+    val SecondaryLight = Color(0xFF00695C)
+    val TertiaryLight = Color(0xFFF9A825)
+    val BackgroundLight = Color(0xFFFAFAF7)
+    val SurfaceLight = Color(0xFFFFFFFF)
+    val SurfaceVariantLight = Color(0xFFECEAE4)
+    val OnBackgroundLight = Color(0xFF1C1B17)
+    val OnSurfaceVariantLight = Color(0xFF5C5B55)
+    val OutlineLight = Color(0xFF918F87)
+
+    val PrimaryDark = Color(0xFF81C784)
+    val OnPrimaryDark = Color(0xFF0A2A0C)
+    val PrimaryContainerDark = Color(0xFF1B5E20)
+    val OnPrimaryContainerDark = Color(0xFFC8E6C9)
+    val SecondaryDark = Color(0xFF4DB6AC)
+    val TertiaryDark = Color(0xFFFFD54F)
+    val BackgroundDark = Color(0xFF121412)
+    val SurfaceDark = Color(0xFF1C1F1C)
+    val SurfaceVariantDark = Color(0xFF262926)
+    val OnBackgroundDark = Color(0xFFE3E2DD)
+    val OnSurfaceVariantDark = Color(0xFFA8A79E)
+    val OutlineDark = Color(0xFF6F6E66)
+
+    val DebtGivenLight = Color(0xFFE53935)
+    val DebtReceivedLight = Color(0xFF43A047)
+    val DebtGivenDark = Color(0xFFEF5350)
+    val DebtReceivedDark = Color(0xFF66BB6A)
+    val OverdueBgLight = Color(0xFFFFF8E1)
+    val OverdueBgDark = Color(0xFF332B14)
+}
