c460debdc559cfb157d92678110f8b1a734aa9dd
umarchon.nurmadov <umarnurmadov12@gmail.com>
2026-07-23 16:25:37 +0500

    refactor(app): удалить дубликат темы, перейти на DaftarTheme из core:designsystem

--- app/src/main/java/tj/daftariqarzho/app/MainActivity.kt
+++ app/src/main/java/tj/daftariqarzho/app/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import tj.daftariqarzho.app.ui.theme.DaftariQarzhoTheme
+import tj.daftariqarzho.app.core.designsystem.theme.DaftarTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DaftariQarzhoTheme {
+            DaftarTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    DaftariQarzhoTheme {
+    DaftarTheme {
         Greeting("Android")
     }
 }
